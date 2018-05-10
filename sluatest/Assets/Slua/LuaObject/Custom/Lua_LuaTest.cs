916834c1082e9a0c31190e004e66b54d7af46887
simonne.xu <min.xu@dena.jp>
2018-05-10 16:02:57 +0800

    add sluatest

---
+++ sluatest/Assets/Slua/LuaObject/Custom/Lua_LuaTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SLua;
+using System.Collections.Generic;
+[UnityEngine.Scripting.Preserve]
+public class Lua_LuaTest : LuaObject {
+	[UnityEngine.Scripting.Preserve]
+	static public void reg(IntPtr l) {
+		getTypeTable(l,"LuaTest");
+		createTypeMetatable(l,null, typeof(LuaTest),typeof(UnityEngine.MonoBehaviour));
+	}
+}
---
+++ sluatest/Assets/Test/LuaTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SLua;
+
+[CustomLuaClass]
+public class LuaTest : MonoBehaviour {
+
+	LuaSvr l;
+	LuaState ls;
+	// Use this for initialization
+	void Start () {
+		l = new LuaSvr();
+        l.init(null, () =>
+        {
+			l.start("LuaTest");
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
